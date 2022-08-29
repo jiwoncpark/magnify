9b51e6ba5b32470d6ac8605654f8070b8596da1e
Ji Won Park <jiwoncpark@gmail.com>
2022-08-29 10:18:17 -0700

    Check in data utils updated for latest magnificat

--- magnify/data/drw_utils.py
+++ magnify/data/drw_utils.py
@@ -5,7 +5,8 @@ ODEs
 import numpy as np
 import torch
 from magnificat.drw_dataset import DRWDataset
-from magnificat.samplers.dc2_sampler import DC2Sampler
+from magnificat.samplers.s82_sampler import S82Sampler
+from magnificat.utils.mag_noise import MagNoise, MagNoiseTorch
 import magnify.data.processing_utils as putils
 
 
@@ -146,51 +147,83 @@ def variable_time_collate_fn(batch, args, device=torch.device("cpu"),
 
 
 def get_drw_datasets(train_seed, val_seed, n_pointings, bandpasses,
-                     t_transform, y_transform,
-                     train_dir, val_dir):
-    train_params = [f'tau_{bp}' for bp in bandpasses]
-    train_params += [f'SF_inf_{bp}' for bp in bandpasses]
-    train_params += ['BH_mass', 'M_i', 'redshift']
-    train_params += [f'mag_{bp}' for bp in bandpasses]
-    log_params = [True for bp in bandpasses]
-    log_params += [True for bp in bandpasses]
-    log_params += [False, False, False]
-    log_params += [False for bp in bandpasses]
+                     t_transform, y_transform, n_train, n_val,
+                     train_dir, val_dir, obs_dir):
+    # Parameter metadata
+    # Must be a subset of dataset.param_names
+    train_params = ['BH_mass', 'M_i']
+    train_params += [f'log_sf_inf_{bp}' for bp in bandpasses]
+    # train_params += [f'{bp}' for bp in bandpasses]
+    train_params += ['redshift']
+    train_params += [f'log_rf_tau_{bp}' for bp in bandpasses]
+    log_params = None
     n_pointings = n_pointings
 
-    train_cat_idx = np.load('/home/jwp/stage/sl/magnify/train_idx.npy')  # 11227
-    val_cat_idx = np.load('/home/jwp/stage/sl/magnify/val_idx.npy')  # 114
-    n_train = len(train_cat_idx)
-    n_val = len(val_cat_idx)
-    train_dataset = DRWDataset(DC2Sampler(train_seed, bandpasses, train_cat_idx),
-                               out_dir=train_dir,
+    train_cat_idx = np.load('/home/jwp/stage/sl/magnify/train_indices.npy')
+    val_cat_idx = np.load('/home/jwp/stage/sl/magnify/val_indices.npy')
+    n_train_total = len(train_cat_idx)  # 4158
+    n_val_total = len(val_cat_idx)  # 460
+    n_train = min(n_train, n_train_total)
+    n_val = min(n_val, n_val_total)
+    print(f"Training size: {n_train}")
+    print(f"Validation size: {n_val}")
+
+    agn_params = ['BH_mass', 'redshift', 'M_i', 'u', 'g', 'r', 'i', 'z']
+    train_sampler = S82Sampler(agn_params=agn_params,
+                         bp_params=['log_rf_tau', 'log_sf_inf'],
+                         bandpasses=list('ugriz'),
+                         out_dir='s82_train_sampler_dir',
+                         seed=123)
+    train_sampler.process_metadata()
+
+    mag_noise_torch = MagNoiseTorch(mag_idx=[0, 1, 2, 3, 4],
+                                    which_bands=list('ugriz'),
+                                    override_kwargs=None,
+                                    depth=10,
+                                    airmass=1.15304)
+    # Now the train_sampler represents training data only
+    train_sampler.idx = train_cat_idx
+    obs_kwargs = dict(n_pointings_init=n_pointings,
+                      obs_dir=obs_dir,
+                      bandpasses=list('ugriz'))
+    # DRWDataset with magnitude noise
+    def y_transform_full(y):
+        return y_transform(mag_noise_torch(y))
+    train_dataset = DRWDataset(train_sampler,
+                               train_dir,
                                num_samples=n_train,
-                               seed=train_seed,
                                is_training=True,
                                transform_x_func=t_transform,
-                               transform_y_func=y_transform,
-                               err_y=0.01,
-                               obs_kwargs={'n_pointings_init': n_pointings,
-                                           'obs_dir': '/home/jwp/stage/sl/magnify/latent_ode_data/gr_obs',
-                                           'bandpasses': bandpasses},
-                               prestored_bandpasses=bandpasses)
+                               transform_y_func=y_transform_full,
+                               prestored_bandpasses=list('ugriz'),
+                               seed=123,
+                               obs_kwargs=obs_kwargs)
+
     train_dataset.slice_params = [train_dataset.param_names.index(n) for n in train_params]
     train_dataset.log_params = log_params
     train_dataset.get_normalizing_metadata(set_metadata=True)
 
     # Validation data
-    val_dataset = DRWDataset(DC2Sampler(val_seed, bandpasses, val_cat_idx),
-                             out_dir=val_dir,
-                             num_samples=n_val,
-                             seed=val_seed,
-                             is_training=False,
-                             transform_x_func=t_transform,
-                             transform_y_func=y_transform,
-                             err_y=0.01,
-                             obs_kwargs={'n_pointings_init': n_pointings,
-                                         'obs_dir': '/home/jwp/stage/sl/magnify/latent_ode_data/gr_obs',
-                                         'bandpasses': bandpasses},
-                             prestored_bandpasses=bandpasses)
+    val_sampler = S82Sampler(agn_params=agn_params,
+                         bp_params=['log_rf_tau', 'log_sf_inf'],
+                         bandpasses=list('ugriz'),
+                         out_dir='s82_train_sampler_dir',
+                         seed=123)
+    val_sampler.process_metadata()
+
+    # Now the val_sampler represents the validation data only
+    val_sampler.idx = val_cat_idx
+    # DRWDataset with magnitude noise
+    val_dataset = DRWDataset(val_sampler,
+                               val_dir,
+                               num_samples=n_val,
+                               is_training=True,
+                               transform_x_func=t_transform,
+                               transform_y_func=y_transform_full,
+                               prestored_bandpasses=list('ugriz'),
+                               seed=123,
+                               obs_kwargs=obs_kwargs)
+
     val_dataset.slice_params = train_dataset.slice_params
     val_dataset.log_params = log_params
     val_dataset.mean_params = train_dataset.mean_params
